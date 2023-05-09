56eb2023dd9aa10441e221a743305ede216e321b
andresk-devt <learningxandres@gmail.com>
2023-05-09 00:02:33 -0500

    Use the react hooks to get the tasks

--- src/components/TodoListComponent.jsx
+++ src/components/TodoListComponent.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/List.css";
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   const getTasks = () => {
     const tasks = JSON.parse(localStorage.getItem("tasks"));
     setTasks(tasks);
-    console.log(tasks, "tasks");
   };
 
   return (
     <>
       <div className="todo-list-container">
         <h1>Todo list</h1>
-        <button onClick={getTasks}>GET TASKS</button>
         <ul className="tasks-list">
           {tasks.map((task) => (
             <li key={task.id}>{ task.title }</li>
