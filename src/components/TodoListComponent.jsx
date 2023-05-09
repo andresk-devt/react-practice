953e8f0b05682c6bafbf10fb16bb901c82a0fc42
andresk-devt <learningxandres@gmail.com>
2023-05-08 23:56:56 -0500

    Get the task list in the todo list component

--- src/components/TodoListComponent.jsx
+++ src/components/TodoListComponent.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import "../styles/List.css";
+
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = () => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+    console.log(tasks, "tasks");
+  };
+
   return (
     <>
-      <h1>Todo list</h1>
+      <div className="todo-list-container">
+        <h1>Todo list</h1>
+        <button onClick={getTasks}>GET TASKS</button>
+        <ul className="tasks-list">
+          {tasks.map((task) => (
+            <li key={task.id}>{ task.title }</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default TodoList;
